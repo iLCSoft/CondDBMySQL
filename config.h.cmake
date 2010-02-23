6c44192b6bf400e969893eddcd73144661e016a0
Angela Lucaci-Timoce <angela-isabela.lucaci-timoce@desy.de>
2010-02-23 15:53:50 +0000

    add config.h.cmake

---
+++ config.h.cmake
@@ -0,0 +1,80 @@
+#cmakedefine CONDDBROOT "${CMAKE_CURRENT_SOURCE_DIR}"
+
+/*defines to 1 if the system is big endian*/
+#cmakedefine CONDDB_BIGENDIAN 
+
+/*define to 1 if you have the <dlfcn.h> header file*/
+#cmakedefine HAVE_DLFCN_H
+
+/*define to 1 if you have the <inttypes.h> header file*/
+#cmakedefine HAVE_INTTYPES_H
+
+/*define to 1 if you have the 'mysqlclient' library (-lmysqlclient)*/
+#cmakedefine HAVE_LIBMYSQLCLIENT
+
+/*define to 1 if you have the 'z' library* (-lz)*/
+#cmakedefine HAVE_LIBZ
+
+/*define to 1 if you have the <memory.h> header file*/
+#cmakedefine HAVE_MEMORY_H
+
+/*define to 1 if you have the <pwd.h> header file*/
+#cmakedefine HAVE_PWD_H
+
+/*define to 1 if you have the <stdint.h> header file*/
+#cmakedefine HAVE_STDINT_H
+
+/*define to 1 if you have the <stdlib.h> header file*/
+#cmakedefine HAVE_STDLIB_H
+
+/*define to 1 if you have the <strings.h> header file*/
+#cmakedefine HAVE_STRINGS_H
+
+/*define to 1 if you have the <string.h> header file*/
+#cmakedefine HAVE_STRING_H
+
+/*define to 1 if you have the <sys/stat.h> header file*/
+#cmakedefine HAVE_SYS_STAT_H
+
+/*define to 1 if you have the <sys/time.h> header file*/
+#cmakedefine HAVE_SYS_TIME_H
+
+/*define to 1 if you have the <sys/types.h> header file*/
+#cmakedefine HAVE_SYS_TYPES_H
+
+/*define to 1 if you have the <time.h> header file*/
+#cmakedefine HAVE_TIME_H
+
+/*define to 1 if you have the <unistd.h> header file*/
+#cmakedefine HAVE_UNISTD_H
+
+/*suffix for lib directories*/
+#cmakedefine MYLIBSUFF ""
+
+/*name of package*/
+#cmakedefine PACKAGE "ConditionsDB"
+
+/*define the address where the bug reports should be sent*/
+#cmakedefine PACKAGE_BUGREPORT ""
+
+/*define the full name of the package*/
+#cmakedefine PACKAGE_NAME ""
+
+/*define the full name and version of this package*/
+#cmakedefine PACKAGE_STRING ""
+
+/*define the one symbol short name of this package*/
+#cmakedefine PACKAGE_TARNAME ""
+
+/*define the  version of this package*/
+#cmakedefine PACKAGE_VERSION ""
+
+/*define the version number of this package*/
+#cmakedefine VERSION "0.4"
+
+
+
+
+
+
+
