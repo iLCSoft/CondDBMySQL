87aed3c992ecc218d4bb8efda0ee5af200784043
Remi Ete <remi.ete@gmail.com>
2019-11-24 18:14:35 +0100

    Adjusted travisCI to find correctly MySQL

---
+++ src/config.h
@@ -0,0 +1,80 @@
+#define CONDDBROOT "/home/local/soft/CondDBMySQL"
+
+/*defines to 1 if the system is big endian*/
+/* #undef CONDDB_BIGENDIAN */
+
+/*define to 1 if you have the <dlfcn.h> header file*/
+#define HAVE_DLFCN_H
+
+/*define to 1 if you have the <inttypes.h> header file*/
+#define HAVE_INTTYPES_H
+
+/*define to 1 if you have the 'mysqlclient' library (-lmysqlclient)*/
+#define HAVE_LIBMYSQLCLIENT
+
+/*define to 1 if you have the 'z' library* (-lz)*/
+#define HAVE_LIBZ
+
+/*define to 1 if you have the <memory.h> header file*/
+#define HAVE_MEMORY_H
+
+/*define to 1 if you have the <pwd.h> header file*/
+#define HAVE_PWD_H
+
+/*define to 1 if you have the <stdint.h> header file*/
+#define HAVE_STDINT_H
+
+/*define to 1 if you have the <stdlib.h> header file*/
+#define HAVE_STDLIB_H
+
+/*define to 1 if you have the <strings.h> header file*/
+#define HAVE_STRINGS_H
+
+/*define to 1 if you have the <string.h> header file*/
+#define HAVE_STRING_H
+
+/*define to 1 if you have the <sys/stat.h> header file*/
+#define HAVE_SYS_STAT_H
+
+/*define to 1 if you have the <sys/time.h> header file*/
+#define HAVE_SYS_TIME_H
+
+/*define to 1 if you have the <sys/types.h> header file*/
+#define HAVE_SYS_TYPES_H
+
+/*define to 1 if you have the <time.h> header file*/
+#define HAVE_TIME_H
+
+/*define to 1 if you have the <unistd.h> header file*/
+#define HAVE_UNISTD_H
+
+/*suffix for lib directories*/
+#define MYLIBSUFF ""
+
+/*name of package*/
+#define PACKAGE "ConditionsDB"
+
+/*define the address where the bug reports should be sent*/
+#define PACKAGE_BUGREPORT ""
+
+/*define the full name of the package*/
+#define PACKAGE_NAME ""
+
+/*define the full name and version of this package*/
+#define PACKAGE_STRING ""
+
+/*define the one symbol short name of this package*/
+#define PACKAGE_TARNAME ""
+
+/*define the  version of this package*/
+#define PACKAGE_VERSION ""
+
+/*define the version number of this package*/
+#define VERSION "0.4"
+
+
+
+
+
+
+
